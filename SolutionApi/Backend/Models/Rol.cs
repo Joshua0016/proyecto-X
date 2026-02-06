ea2598e6179ee4213118a8a495f3d927d0ab1406
Morrobel07 <morrobelc03@gmail.com>
2026-02-05 21:00:58 -0400

    second

---
+++ SolutionApi/Backend/Models/Rol.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Rol
+    {
+        [Key]
+        public int IdRol { get; set; }
+
+        [Required, StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        public string? Descripcion { get; set; }
+
+        public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+
+    }
+}
---
+++ SolutionApi/Backend/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public int IdRol { get; set; }
+
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("IdRol")]
+        public virtual Rol? Rol { get; set; } = null;
+    }
+}
