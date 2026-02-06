06bc0424d2891cbde0167ac20fcf3a1f7607661c
Morrobel07 <morrobelc03@gmail.com>
2026-02-05 21:39:14 -0400

    second

--- SolutionApi/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ SolutionApi/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2b6d5af1eff4922bfca8d95b9b4ca0ebfc312d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea2598e6179ee4213118a8a495f3d927d0ab1406")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
