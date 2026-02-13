890b2a77b296e9b9db7388a52597c194658338a3
Joshua <joshuanivar16@gmail.com>
2026-02-12 23:30:06 -0400

    conection backend pending

--- frontend/src/api/login.js
+++ frontend/src/api/login.js
@@ -1,5 +1,33 @@
 //Pendiente autenticacion del login
 export default async function login(username, password) {
-    console.log(username, password);
+
+    try {
+        let request = {
+            Email: username,
+            Password: password
+        }
+        let response = await fetch("/api/Auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(request)
+        });
+
+        if (response.ok) {
+            let request = await response.json();
+            return request;
+        }
+        else {
+            console.log("error al enviar la peticon");
+            return false;
+        }
+
+    } catch (error) {
+
+        console.log("Error en el try catch ", error);
+
+    }
+
     return true;
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        secure: false,
+        target: "https://localhost:44353",
+        changeOrigin: true
+      }
+    }
+  }
 })
